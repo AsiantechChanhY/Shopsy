12b96c1639163288f2ac9e44eac38d9849ae4274
chanhy <tnycit1993@gmail.com>
2016-04-26 17:35:38 +0700

    sh

---
+++ app/src/main/java/com/example/chanhy/shopsy/SplashPage.java
@@ -0,0 +1,18 @@
+package com.example.chanhy.shopsy;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * @author Prem Kumar
+ *
+ */
+public class SplashPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splashpage);
+
+	}
+}
